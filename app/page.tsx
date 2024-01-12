4b4a2d3a7c3dade1ba55a772c2fb97829de222fe
Don Kassim <don@donkassim.com>
2024-01-12 18:14:00 +0300

    working

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Lusitana } from 'next/font/google';
 import Image from 'next/image';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
